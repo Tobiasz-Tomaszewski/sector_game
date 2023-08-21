6a34ec878092010e89a6b356c67733fbd9ca0b9e
Tobiasz-Tomaszewski <tpt484@poczta.onet.pl>
2023-08-21 17:36:16 +0200

    WIP: Creating and testing SurfaceHandler class.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 # pygame setup
 pygame.init()
 
-
+global height, width
 height, width = 1280, 720
 
 screen = pygame.display.set_mode((height, width))
@@ -13,32 +13,84 @@ screen = pygame.display.set_mode((height, width))
 centre = screen.get_width() / 2, screen.get_height() / 2
 
 
+class SurfaceHandler:
+    def __init__(self):
+        self.surfaces = {}
+
+    def create_surface(self, height, width):
+        surface_name = self.create_available_name()
+        self.add_surface(surface_name, pygame.Surface((height, width), pygame.SRCALPHA))
+        return surface_name
+
+    def add_surface(self, surface_name, surface):
+        self.surfaces[surface_name] = surface
+
+    def get_surface(self, surface_name):
+        return self.surfaces[surface_name]
+
+    def create_available_name(self):
+        current_indexes = sorted(int(x) for x in self.surfaces.keys())
+        all_indexes = [x for x in range(len(self.surfaces))]
+        zipped = zip(current_indexes, all_indexes)
+        unused_indexes = [y for x, y in zipped if x != y]
+        if unused_indexes:
+            return str(min(unused_indexes))
+        return '0'
+
+
 class Obstacle:
-    def __init__(self, inner_radius, outer_radius, start_angle, angle):
-        self.inner_radius = inner_radius
-        self.outer_radius = outer_radius
+    def __init__(self, start_angle, angle, speed = 1):
+        # Obstacle will start on the edge of the screen.
+        self.inner_radius = math.sqrt((centre[0])**2 + (centre[1])**2)
+        self.outer_radius = math.sqrt((centre[0])**2 + (centre[1])**2) + 10
         self.start_angle = start_angle
         self.angle = angle
         self.centre = centre
+        self.speed = speed
 
-    def get_polygon_points(self):
+    def create_polygon_points(self, radius):
         # Start list of polygon points
         polygon_points = [self.centre]
         # Get points on arc
         for n in range(0, int(self.angle)):
-            x = self.centre[0] + int(r * math.cos(n * math.pi / 180))
-            y = self.centre[1] + int(r * math.sin(n * math.pi / 180))
+            x = self.centre[0] + int(radius * math.cos(n * math.pi / 180))
+            y = self.centre[1] + int(radius * math.sin(n * math.pi / 180))
             polygon_points.append((x, y))
         polygon_points.append(self.centre)
         return polygon_points
 
-    def create_pseudo_sector_of_the_ring(self):
+    def create_sector_of_the_ring_points(self):
+        outer_points = self.create_polygon_points(self.outer_radius)
+        inner_points = self.create_polygon_points(self.inner_radius)
+        return outer_points, inner_points
+
+    def move_obstacle(self):
+        self.inner_radius -= self.speed
+        self.outer_radius -= self.speed
+
+    def create_sector_of_the_ring(self, SurfaceHandler, ObstacleHandler):
+        outer_points, inner_points = self.create_sector_of_the_ring_points()
+        SurfaceHandler.create_surface()
+
         pos1, rad1 = centre, 100
         pos2, rad2 = centre, 110
         surf1 = pygame.Surface((height, width), pygame.SRCALPHA)
         surf2 = pygame.Surface((height, width), pygame.SRCALPHA)
         surf2.blit(surf1, (0, 0), special_flags=pygame.BLEND_RGBA_SUB)
 
+        if len(self.polygon_points) > 2:
+            pygame.draw.polygon(screen, (0, 0, 0), self.polygon_points)
+
+        pygame.draw.circle(surf1, (255, 0, 0, 255), pos1, rad1)
+        pygame.draw.circle(surf2, (255, 0, 0, 255), pos2, rad2)
+        surf2.blit(surf1, (0, 0), special_flags=pygame.BLEND_RGBA_SUB)
+        screen.blit(surf2, (0, 0))
+        surf2.fill(pygame.Color(0, 0, 0, 0))
+        surf1.fill(pygame.Color(0, 0, 0, 0))
+
+
+    def rotate_obstacle(self, rotation_angle):
+        pass
 
     def move_obstacle(self):
         pass
@@ -51,7 +103,7 @@ class ObstacleHandler:
     def __init__(self):
         pass
 
-    def add_obstacle(self):
+    def add_obstacle(self, obstacle):
         pass
 
     def draw_obstacles(self, screen):
@@ -72,12 +124,11 @@ pygame.draw.circle(screen, (17, 153, 255), (cx, cy), r)
 clock = pygame.time.Clock()
 running = True
 dt = 0
-player_pos = pygame.Vector2(centre[0], centre[1])
+player_pos = pygame.Vector2(0, 0)
 pos1, rad1 = centre, 100
 pos2, rad2 = centre, 110
 surf1 = pygame.Surface((height, width), pygame.SRCALPHA)
 surf2 = pygame.Surface((height, width), pygame.SRCALPHA)
-surf2.blit(surf1, (0, 0), special_flags=pygame.BLEND_RGBA_SUB)
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -102,12 +153,22 @@ while running:
     if keys[pygame.K_DOWN]:
         rad1 = rad1 - 1
         rad2 = rad2 - 1
+
+
+    test_surface_handler = SurfaceHandler()
+    create_test_surface = test_surface_handler.create_surface(height, width)
+    pygame.draw.circle(test_surface_handler.get_surface(create_test_surface), (255, 0, 0, 255), pos2, 10)
+    screen.blit(test_surface_handler.get_surface(create_test_surface), (0, 0))
+
+
     pygame.draw.circle(surf1, (255, 0, 0, 255), pos1, rad1)
     pygame.draw.circle(surf2, (255, 0, 0, 255), pos2, rad2)
     surf2.blit(surf1, (0, 0), special_flags=pygame.BLEND_RGBA_SUB)
     screen.blit(surf2, (0, 0))
     surf2.fill(pygame.Color(0, 0, 0, 0))
     surf1.fill(pygame.Color(0, 0, 0, 0))
+
+
 
 ####    if len(p) > 2:
 ####        pygame.draw.polygon(screen, (0, 0, 0), p)
